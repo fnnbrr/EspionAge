ba7610972fe9f8f9a9f879737b52c9cd86e42d7e
Alexander Jurcau <jurcaua@gmail.com>
2020-01-18 12:28:33 -0500

    Player and Camera controller basic mechanics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public float turnSpeed = 20f;
+    public float canMoveRotationThreshold = 0.1f;
+
+    private Rigidbody m_Rigidbody;
+
+    private Vector3 m_Movement;
+    private Quaternion m_Rotation;
+
+    void Start()
+    {
+        m_Rigidbody = Utils.GetRequiredComponent<Rigidbody>(this);
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;    // to be used later for animations and such
+
+        HandleControl(m_Movement);
+    }
+
+    void HandleControl(Vector3 movementDirection)
+    {
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movementDirection, turnSpeed * Time.fixedDeltaTime, 0f);
+
+        HandleRotation(desiredForward);
+
+        // Only start moving once we are close enough to our desired final rotation (which we are smoothly rotating towards)
+        if ((movementDirection - desiredForward).magnitude < canMoveRotationThreshold)
+        {
+            HandleMovement();
+        }
+    }
+
+    void HandleRotation(Vector3 desiredForward)
+    {
+        m_Rigidbody.MoveRotation(Quaternion.LookRotation(desiredForward));
+    }
+
+    void HandleMovement()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+    public static T GetRequiredComponent<T>(MonoBehaviour o) where T : Component
+    {
+        T comp = o.GetComponent<T>();
+        if (!comp)
+        {
+            Debug.LogError($"Expected component '{typeof(T).Name}' on object {o.name}");
+        }
+        return comp;
+    }
+}
