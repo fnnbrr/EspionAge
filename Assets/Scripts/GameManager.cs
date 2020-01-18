50a9449fab48819e7c743b66161015520b7e43d1
Alexander Jurcau <jurcaua@gmail.com>
2020-01-18 18:05:40 -0500

    new UIManager singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class GameManager : Singleton<GameManager>
 {
-    public GameObject canRestUI;
-    public TextMeshProUGUI restText;
-
-    private string defaultRestText;
-    private void Start()
-    {
-        defaultRestText = restText.text;
-    }
-
-    //// TEMP FUNCTION - Move to UIManager later
-    public void EnableCanRestUI(bool toEnable)
-    {
-        canRestUI.SetActive(toEnable);
-    }
-
-    // TEMP FUNCTION - Move to UIManager later
-    public void UpdateRestingText(bool isResting)
-    {
-        restText.text = (isResting ? "Resting..." : defaultRestText);
-    }
+    
 }
