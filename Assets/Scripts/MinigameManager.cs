78479cedf9b0bb9a4a77157a39aa2f5c379ca285
Alexander Jurcau <jurcaua@gmail.com>
2020-01-20 00:12:18 -0500

    Resting Camera Blending + Hookup w/ Placeholder Animations

--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -14,6 +14,10 @@ public class MinigameManager : Singleton<MinigameManager>
     private bool isInMinigame = false;
     private Scene currentMinigameScene;
 
+    // Public events that can be subscribed to
+    public delegate void MinigameCompleteAction();
+    public event MinigameCompleteAction OnMinigameComplete;
+
     public bool IsInMinigame()
     {
         return isInMinigame && currentMinigameScene.IsValid();
@@ -23,7 +27,7 @@ public class MinigameManager : Singleton<MinigameManager>
     {
         return minigameScenes[Random.Range(0, minigameScenes.Length)];
     }
-
+    
     public void LoadRandomMinigame()
     {
         if (minigameScenes.Length == 0)
@@ -84,6 +88,9 @@ public class MinigameManager : Singleton<MinigameManager>
         //    - possible fix... keep track of objects that were already inactive and just make sure they are not reactivated later...?
         //    - we can also just add those specific objects in the ignore list and then they will not be an issue at all
         SetActiveAllObjectsInScene(SceneManager.GetActiveScene(), true);
+
+        // Alert anyone waiting for the minigame to be complete
+        OnMinigameComplete?.Invoke();
     }
 
     private void SetActiveAllObjectsInScene(Scene scene, bool active)
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerManager : MonoBehaviour
 {
     [Header("Stamina")]
     public float staminaIncrease = 0.1f;
     public float staminaDecrease = 0.001f;
+
+    [Header("Player Camera")]
+    public CinemachineVirtualCamera playerCamera;
 
     [SerializeField]  // this allows us to see the field update in the inspector (helps for debugging) 
     private bool _canRest;
@@ -18,6 +22,17 @@ public class PlayerManager : MonoBehaviour
             UIManager.Instance.EnableCanRestUI(value);
             _canRest = value; 
         }
+    }
+
+    private bool isInMinigame = false;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = Utils.GetRequiredComponent<Animator>(this);
+
+        MinigameManager.Instance.OnMinigameComplete += HandleMinigameComplete;
     }
 
     private void Update()
@@ -33,10 +48,13 @@ public class PlayerManager : MonoBehaviour
             } 
             else
             {
-                if (Input.GetKeyDown(KeyCode.P))
+                if (Input.GetKeyDown(KeyCode.P) && !isInMinigame)
                 {
+                    isInMinigame = true;
                     // Temporary Controls for Minigame
-                    MinigameManager.Instance.LoadRandomMinigame();
+                    // The player being in a resting state will also trigger the state-driven camera to also zoom into the player
+                    animator.SetBool(Constants.ANIMATION_PLAYER_ISRESTING, true);
+                    StartCoroutine(WaitForCameraSwitch());
                 }
             }
         }
@@ -44,6 +62,23 @@ public class PlayerManager : MonoBehaviour
         {
             UIManager.Instance.UpdateRestingText(false);
         }
+    }
+
+    IEnumerator WaitForCameraSwitch()
+    {
+        // https://forum.unity.com/threads/how-can-i-tell-when-ive-reached-my-active-virtual-cam-blend-has-completed.498544/
+        // Because we blend between two cameras, the player cam is live only until the blending has completed
+        while (CinemachineCore.Instance.IsLive(playerCamera))
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        MinigameManager.Instance.LoadRandomMinigame();
+    }
+
+    void HandleMinigameComplete()
+    {
+        animator.SetBool(Constants.ANIMATION_PLAYER_ISRESTING, false);
+        isInMinigame = false;
     }
 
     void HandleIncreaseStamina()
