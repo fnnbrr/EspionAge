faa5c2802ff6bcacf81710526e631531893adee7
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-01-21 17:31:47 -0500

    Made a parent class for item that currently contain the ontriggerstay, acquire and drop functions. Also made an interface for items class

---
+++ Assets/Scripts/Items/IItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Application
+{
+    public interface IItem
+    {
+        void OnTriggerStay(Collider other);
+
+        void AcquireItem(Transform player);
+
+        void DropItem();
+
+        void Effect();
+
+    }
+}
---
+++ Assets/Scripts/Items/ItemWalker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Application;
+using UnityEngine;
+
+public class ItemWalker : Item
+{
+    public float distanceFromCenter = 2.0f;
+
+
+    // Sets the position of the item relative to the player
+    public override void AcquireItem(Transform player)
+    {
+        base.AcquireItem(player);
+        gameObject.GetComponent<BoxCollider>().isTrigger = false;
+        SetHeldPosition(player);
+        //Effect()
+    }
+
+
+    public override void DropItem()
+    {
+        base.DropItem();
+        gameObject.GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    // While item is held, apply its effect (if any)
+    public new void Effect()
+    {
+
+    }
+
+    public void SetHeldPosition(Transform player)
+    {
+        gameObject.transform.position = new Vector3(player.position.x, gameObject.transform.position.y, player.position.z) + player.forward * distanceFromCenter;
+
+        gameObject.transform.rotation = player.rotation;
+
+    }
+
+}
