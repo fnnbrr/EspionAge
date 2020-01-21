52de187177b986fd5139446679bcaeec689562cb
Alexander Jurcau <jurcaua@gmail.com>
2020-01-21 12:12:50 -0500

    Big StaminaBarSetup simplification + cleaning up UI resource dir structure

--- Assets/Scripts/Minigames/MinigameBarSetup.cs
+++ Assets/Scripts/Minigames/MinigameBarSetup.cs
@@ -3,18 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-class MinigameInstantiateLater
-{
-    public Sprite sprite;
-    public string name;
-
-    public MinigameInstantiateLater(Sprite s, string n)
-    {
-        sprite = s;
-        name = n;
-    }
-}
-
 [ExecuteAlways]
 public class MinigameBarSetup : MonoBehaviour
 {
@@ -22,13 +10,8 @@ public class MinigameBarSetup : MonoBehaviour
 
     [Header("Preset Assets")]
     public GameObject segmentBasePrefab;
-
-    public Sprite startSegmentSprite;
-    private readonly string startSegmentName = "StartSegment";
-    public Sprite middleSegmentSprite;
-    private readonly string middleSegmentName = "MiddleSegment";
-    public Sprite endSegmentSprite;
-    private readonly string endSegmentName = "EndSegment";
+    public Sprite segmentSprite;
+    private readonly string segmentName = "Segment";
 
     public enum MinigameStaminaType
     {
@@ -37,7 +20,7 @@ public class MinigameBarSetup : MonoBehaviour
         Target
     }
     public MinigameStaminaType staminaType;
-    public Sprite segmentSprite;
+    public Sprite segmentSubSprite;
     public List<Image> childImages;
 
     private void OnValidate()
@@ -63,54 +46,42 @@ public class MinigameBarSetup : MonoBehaviour
         }
         else
         {
-            List<MinigameInstantiateLater> toInstantiateLater = new List<MinigameInstantiateLater>();
+            int toInstantiateLater = 0;
 
             List<Image> list = new List<Image>();
-            Image[] kids = staminaFillTransform.GetComponentsInChildren<Image>(true);
-            foreach (Image k in kids)
+            foreach (Transform k in staminaFillTransform)
             {
-                if (k.transform.parent == staminaFillTransform)
+                Image kImage = k.GetComponent<Image>();
+                if (kImage)
                 {
-                    list.Add(k);
-                }
-            }
-            if (list.Count > 0)
-            {
-                if (list[0].sprite != startSegmentSprite)
-                {
-                    list[0].sprite = startSegmentSprite;
-                }
-                list[0].name = startSegmentName;
-
-                if (list.Count > 1)
-                {
-                    for (int i = 1; i < list.Count - 1; i++)
-                    {
-                        if (list[i].sprite != middleSegmentSprite)
-                        {
-                            list[i].sprite = middleSegmentSprite;
-                        }
-                        list[i].name = middleSegmentName;
-                    }
-
-                    if (list[list.Count - 1].sprite != endSegmentSprite)
-                    {
-                        list[list.Count - 1].sprite = endSegmentSprite;
-                    }
-                    list[list.Count - 1].name = endSegmentName;
+                    list.Add(kImage);
                 }
                 else
                 {
-                    toInstantiateLater.Add(CreateEndSegment());
+                    // Destroy anything that does not have an image component
+                    DestroyAtEndOfFrame(k.gameObject);
                 }
             }
+
+            // Let's force the count to be at least length 2
+            if (list.Count < 2)
+            {
+                while(list.Count + toInstantiateLater < 2)
+                {
+                    toInstantiateLater++;
+                }
+            } 
+            // Otherwise, check to make sure all have the correct sprite and name
             else
             {
-                toInstantiateLater.Add(CreateStartSegment());
-                toInstantiateLater.Add(CreateEndSegment());
+                foreach(Image image in list)
+                {
+                    image.sprite = segmentSprite;
+                    image.name = segmentName;
+                }
             }
 
-            if (toInstantiateLater.Count > 0)
+            if (toInstantiateLater > 0)
             {
                 // because we cannot instantiate objects in OnValidate (because it called SendMessage in the background), 
                 //  we wait until the end of the frame for this (like deleting objects):
@@ -122,12 +93,12 @@ public class MinigameBarSetup : MonoBehaviour
         }
     }
 
-    IEnumerator InstantiateAtEndOfFrame(List<MinigameInstantiateLater> instLater)
+    IEnumerator InstantiateAtEndOfFrame(int num)
     {
         yield return new WaitForEndOfFrame();
-        foreach (MinigameInstantiateLater toInst in instLater)
+        for (int i = 0; i < num; i++)
         {
-            ImageCreateImage(toInst.sprite, toInst.name);
+            ImageCreateImage();
         }
         UpdateListOfChildren();
     }
@@ -138,32 +109,17 @@ public class MinigameBarSetup : MonoBehaviour
         DestroyImmediate(go);
     }
 
-    private MinigameInstantiateLater CreateStartSegment()
-    {
-        return new MinigameInstantiateLater(startSegmentSprite, startSegmentName);
-    }
-
-    private MinigameInstantiateLater CreateMiddleSegment()
-    {
-        return new MinigameInstantiateLater(middleSegmentSprite, middleSegmentName);
-    }
-
-    private MinigameInstantiateLater CreateEndSegment()
-    {
-        return new MinigameInstantiateLater(endSegmentSprite, endSegmentName);
-    }
-
-    private Image ImageCreateImage(Sprite sprite, string name)
+    private Image ImageCreateImage()
     {
         GameObject segment = Instantiate(segmentBasePrefab, staminaFillTransform);
-        segment.name = name;
+        segment.name = segmentName;
         Image segmentImage = segment.GetComponent<Image>();
-        segmentImage.sprite = sprite;
+        segmentImage.sprite = segmentSprite;
         
         if (staminaType.Equals(MinigameStaminaType.Target))
         {
-            Image segmentSubSprite = segmentImage.gameObject.GetComponentInChildren<Image>();
-            segmentSubSprite.sprite = segmentSprite;
+            Image segmentSubImage = segmentImage.gameObject.GetComponentInChildren<Image>();
+            segmentSubImage.sprite = segmentSubSprite;
         }
         return segmentImage;
     }
