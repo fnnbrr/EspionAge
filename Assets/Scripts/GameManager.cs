2f9bff000fa45ac8228101dfb8b913a750875226
Alexander Jurcau <jurcaua@gmail.com>
2020-01-28 12:47:01 -0500

    Can Start Mission (Debug) + Mission Prefabs Created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,12 @@ public class GameManager : Singleton<GameManager>
 
     private PlayerManager playerManager;
     private PlayerController playerController;
+
+    // TODO: Remove below once Dialogue + Triggering Mission start is implemented
+    [Header("Remove below once we have dialogue")]
+    public bool testStartMission = false;
+    public GameObject missionPrefab;
+
     private void Start()
     {
         if (!player)
@@ -17,6 +23,16 @@ public class GameManager : Singleton<GameManager>
 
         playerManager = player.GetComponent<PlayerManager>();
         playerController = player.GetComponent<PlayerController>();
+
+        ////////////////////////////////////////////////////
+        // TOOD: REMOVE ONCE DIALOGUE IS IMPLEMENTED
+        // - This is here instead of the MissionManager to test calling this from another class
+        //   - (like Dialogue eventually will)
+        if (testStartMission && missionPrefab)
+        {
+            MissionManager.Instance.StartMission(missionPrefab);
+        }
+        ////////////////////////////////////////////////////
     }
 
     public Transform GetPlayerTransform()
