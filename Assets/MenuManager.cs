12fe16cb10929512a1cb743874571d2ed4d97001
Alexander Jurcau <jurcaua@gmail.com>
2020-01-31 19:01:46 -0500

    Camera Blending Around Player on Start in Main Menu

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -4,41 +4,78 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class MenuManager : MonoBehaviour
 {
     public Light mainLight;
+    
+    [Header("Image Darkening")]
     public Color imageDarkenedColor;
-    public List<Image> imagesToDarknen;
+    public List<Graphic> imagesToDarknen;
     private List<Color> imagesOriginalColor = new List<Color>();
+
+    [Header("Camera Switching")]
+    public List<CinemachineVirtualCamera> onPressPlayCameras;
+
+    private bool isStarting;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
 
-    public void LoadMain()
+    public void HandlePressStart()
+    {
+        isStarting = true;
+
+        if (onPressPlayCameras.Count > 0)
+        {
+            CameraManager.Instance.OnBlendingComplete += HandlePressStartCameraBlendingComplete;
+            CameraManager.Instance.BlendTo(onPressPlayCameras[0]);
+            onPressPlayCameras.RemoveAt(0);
+        } 
+        else
+        {
+            StartMainScene();
+        }
+    }
+
+    private void HandlePressStartCameraBlendingComplete(CinemachineVirtualCamera fromCamera, CinemachineVirtualCamera toCamera)
+    {
+        CameraManager.Instance.OnBlendingComplete -= HandlePressStartCameraBlendingComplete;
+        HandlePressStart();
+    }
+
+    void StartMainScene()
     {
         SceneManager.LoadScene(Constants.SCENE_MAIN);
     }
 
     public void OnStartButtonEnter()
     {
-        mainLight.enabled = false;
+        if (!isStarting)
+        {
+            mainLight.enabled = false;
 
-        imagesOriginalColor.Clear();
-        imagesToDarknen.ForEach(i => {
-            imagesOriginalColor.Add(i.color);
-            i.color = imageDarkenedColor; 
-        });
+            imagesOriginalColor.Clear();
+            imagesToDarknen.ForEach(i =>
+            {
+                imagesOriginalColor.Add(i.color);
+                i.color = imageDarkenedColor;
+            });
+        }
     }
 
     public void OnStartButtonExit()
     {
-        mainLight.enabled = true;
+        if (!isStarting)
+        {
+            mainLight.enabled = true;
 
-        imagesToDarknen.ForEach(i => {
-            i.color = imagesOriginalColor[imagesToDarknen.IndexOf(i)];
-        });
+            imagesToDarknen.ForEach(i => {
+                i.color = imagesOriginalColor[imagesToDarknen.IndexOf(i)];
+            });
+        }
     }
 }
