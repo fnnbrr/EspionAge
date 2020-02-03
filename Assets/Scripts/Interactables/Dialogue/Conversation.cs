7f771d5321699e71afb7fa5acc1368c96ec91f23
Justine Chen <justinechen619@gmail.com>
2020-02-03 10:23:47 -0500

    Added Speaker/Conversation scriptable objects

---
+++ Assets/Scripts/Interactables/Dialogue/Conversation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Line
+{
+    public Speaker speaker;
+
+    [TextArea(2, 5)]
+    public string text;
+}
+
+[CreateAssetMenu(fileName = "New Conversation", menuName = "Conversation")]
+public class Conversation : ScriptableObject
+{
+    public Speaker Birdie;
+    public Speaker NPC;
+    public Line[] lines;
+}
---
+++ Assets/Scripts/Interactables/Dialogue/Speaker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Speaker", menuName = "Speaker")]
+public class Speaker : ScriptableObject
+{
+    public string speaker;
+}
