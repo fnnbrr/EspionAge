2e69e7e765793c82abf8c3059f2a5bcdf3ffa45b
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-02-04 20:25:09 -0500

    Added bingo ball collectible script and bingo ball functions in ProgressManager

--- Assets/Scripts/ProgressManager.cs
+++ Assets/Scripts/ProgressManager.cs
@@ -14,6 +14,9 @@ public class ProgressManager: Singleton<ProgressManager>
     public List<Collectible> stampCollectibles;                        // List of collectibles needed to be able to add stamps in the inspector
     public Dictionary<Collectible, bool> stampsUnlockStatus;           // Use a dictinoary to keep track of what stamps are unlocked
 
+    public List<Collectible> bingoBallCollectibles;                    // List of collectibles needed to be able to add bingo balls in the inspector
+    public Dictionary<Collectible, bool> bingoBallsUnlockStatus;        // Keeps track of unlock status for bingo balls
+
 
     void Start()
     {
@@ -33,7 +36,7 @@ public class ProgressManager: Singleton<ProgressManager>
     {
         if(!stampsUnlockStatus.ContainsKey(stamp))
         {
-            Debug.Log("Cannot find stamp collectible from all unlockable stamps");
+            Debug.LogError("Cannot find stamp collectible from all unlockable stamps");
             return; 
         }
         else
@@ -70,6 +73,36 @@ public class ProgressManager: Singleton<ProgressManager>
 
         return availableStamps;
     }
+
+
+    public void UnlockBingoBallCollectible(Collectible bingoBall)
+    {
+        if (!bingoBallsUnlockStatus.ContainsKey(bingoBall))
+        {
+            Debug.LogError("Cannot find stamp collectible from all unlockable bingo balls");
+            return;
+        }
+        else
+        {
+            bingoBallsUnlockStatus[bingoBall] = true;
+            Debug.Log("Unlocked " + bingoBall.collectibleName);
+        }
+    }
+
+
+    public bool HasUnlockedBingoBall(Collectible bingoBall)
+    {
+        if(!bingoBallsUnlockStatus.ContainsKey(bingoBall))
+        {
+            Debug.LogError("Bingo ball does not exist");
+            return false;
+        }
+        else
+        {
+            return bingoBallsUnlockStatus[bingoBall];
+        }
+    }
+
 
     // To be called in Mission Manager when mission is completed
     public void AddCompletedMission(InProgressMissionContainer completedMission)
