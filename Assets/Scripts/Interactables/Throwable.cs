c94d0f62c2537c90625006e3383f43380e4e5f96
Finnbarr O'Callahan <lukasocallahan@gmail.com>
2020-02-18 21:31:17 -0500

    Added visual ping to throwables hitting colliders

--- Assets/Scripts/Interactables/Throwable.cs
+++ Assets/Scripts/Interactables/Throwable.cs
@@ -1,17 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Throwable : Interactable
 {
+    public GameObject pingPrefab;
+    public float pingFloorOffset = 1.0f;
+    private bool hasBeenAcquired = false;
+    private bool hasHit = false;
+    
     public override void OnInteract()
     {
         base.OnInteract();
 
         PlayerManager playerManager = Utils.GetRequiredComponent<PlayerManager>(player);
         playerManager.AddThrowable(gameObject);
+        hasBeenAcquired = true;
 
         // TODO: Play object pickup sound effect here?
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!hasBeenAcquired || hasHit) return;
+        
+        Vector3 hitPoint = other.GetContact(0).point;
+        Vector3 hitNormal = other.GetContact(0).normal;
+        
+        Instantiate(pingPrefab, hitPoint + (pingFloorOffset * hitNormal), 
+            Quaternion.LookRotation(-hitNormal));
+        hasHit = true;
     }
 }
