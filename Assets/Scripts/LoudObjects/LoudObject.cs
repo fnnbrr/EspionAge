8b1b535fa3712193c325ff9c8d651dad4f1c80f0
Justine Chen <justinechen619@gmail.com>
2020-02-19 16:01:39 -0500

    Updated loud object with distance checking instead of collider and gizmos

--- Assets/Scripts/LoudObjects/LoudObject.cs
+++ Assets/Scripts/LoudObjects/LoudObject.cs
@@ -5,25 +5,86 @@ using UnityEngine;
 
 public class LoudObject : MonoBehaviour
 {
-    public float thrust = 1.0f;
-    public Rigidbody rb;
-    // Start is called before the first frame update
+    public float thrust;
+    public Vector3 direction;
+    public float dropRadius;
+    public float innerDropRadius;
+    public float loudObjectRadius;
+    private float distance;
+    private Rigidbody rb;
+
+    public float minShake;
+    public float maxShake;
+    Renderer[] rends;
+    
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
+        rends = GetComponents<Renderer> ();
     }
 
-    protected void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer(Constants.LAYER_PLAYER))
+        distance = Vector3.Distance(transform.position, GameManager.Instance.GetPlayerTransform().position);
+        if (distance <= innerDropRadius)
         {
-            print(other.transform);
-            print("UR IN BRO");
-            // wobble wobble trigger alex's shader
-            // make object fall 
-            rb.AddForce(transform.forward * 180);
+            rb.AddForce(direction * thrust);
             rb.useGravity = true;
+            NotifyChasingNurse();
+        } 
+        else if (distance <= (innerDropRadius + 3))
+        {
+            Shake(maxShake);
+        } 
+        else if (distance <= dropRadius)
+        {
+            float dynamicShake = Mathf.Lerp(maxShake, minShake, Mathf.Clamp(distance/5.0f, 0f, 1f));
+            Shake(dynamicShake);
+        } 
+        else 
+        {
+            Shake(0f);
         }
-
     }
+
+    void NotifyChasingNurse()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, loudObjectRadius);
+        int i = 0;
+        foreach (Collider collider in hitColliders)
+        {
+            // if collider is the chasing nurse 
+            // {
+            // call it's function 
+            // }
+        }
+    }
+
+    void Shake(float shake){
+        foreach (Renderer rend in rends){
+            if (rend.material.shader == Shader.Find("PlateShake")){
+                // SHAKE SHAKE SHAKE SHAKE
+                rend.material.SetFloat("_ShakeAmplitude", shake);
+            }
+        }
+    }
+    
+    void OnDrawGizmos()    
+    {
+        // Direction arrow of object falling
+        Gizmos.color = Color.red;
+        Vector3 directionToDraw = transform.TransformDirection(direction) * 5;
+        Gizmos.DrawRay(transform.position, directionToDraw);
+
+        // Radius sphere 
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, dropRadius);
+
+        // Loud object radius
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, loudObjectRadius);
+    }
+
 }
+
