afe04340b0bc35b27c0f4484da0f400102b101a6
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-02-26 00:17:22 -0500

    Added pill bottle count to appear in UI

---
+++ Assets/Scripts/ItemThrowCount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ItemThrowCount : MonoBehaviour
+{
+    private int numItemsHeld = 0;
+    PlayerManager playerManager;
+    TextMeshProUGUI text;
+
+
+    void Start()
+    {
+        playerManager = GameManager.Instance.GetPlayerManager();
+
+        if(playerManager == null)
+        {
+            Debug.LogError("Player manager does not exist");
+        }
+
+        playerManager.OnThrow += HandleOnThrewBottle;
+        playerManager.OnPickup += OnHandlePickup;
+
+        text = Utils.GetRequiredComponentInChildren<TextMeshProUGUI>(this);
+
+        if(text == null)
+        {
+            Debug.LogError("Throwable Object text is null");
+        }
+        text.SetText("x" + numItemsHeld.ToString());
+    }
+
+    void OnHandlePickup(GameObject source)
+    {
+        numItemsHeld += 1;
+        DisplayUpdate();
+    }
+
+    void HandleOnThrewBottle(Interactable source)
+    {
+        numItemsHeld -= 1;
+        DisplayUpdate();
+    }
+
+    void DisplayUpdate()
+	{
+        text.SetText("x" + numItemsHeld.ToString());
+	}
+}
