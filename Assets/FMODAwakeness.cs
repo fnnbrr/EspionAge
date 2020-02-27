749c2661505997204a069000663dee993cf277c2
masonachu <masonachu@gmail.com>
2020-02-27 10:45:35 -0500

    Added FMODAwakeness script and additional music stingers

---
+++ Assets/FMODAwakeness.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODAwakeness : MonoBehaviour
+{
+    [FMODUnity.ParamRef]
+    public string parameter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager.Instance.staminaBar.OnChange += UpdateFMODAudio;
+    }
+
+    private void UpdateFMODAudio(float fillAmount)
+    {
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName(parameter, fillAmount);
+    }
+}
