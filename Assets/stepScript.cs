7154b11d41bdcb38c8ec72dd263dfd2279539845
Alexander Jurcau <jurcaua@gmail.com>
2020-02-27 17:39:08 -0500

    Update Assets/stepScript.cs

--- Assets/stepScript.cs
+++ Assets/stepScript.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class stepScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Step(string path)
