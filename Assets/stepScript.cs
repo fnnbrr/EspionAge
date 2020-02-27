5dd545f7402bb50932860a2a03c900b8ccd7b572
masonachu <masonachu@gmail.com>
2020-02-27 17:31:56 -0500

    fmod footsteps added
    finally holy shit

---
+++ Assets/stepScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stepScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Step(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path);
+    }
+}
