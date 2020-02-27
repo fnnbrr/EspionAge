cfefe416aa2ccc8945ec0e18f2424f26b4a36a0d
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-02-26 19:12:24 -0500

    Refactored MissionManager and Interactble Scripts to use Mission Enums instead of AMission to reference to mission. Created StampProgressManager which keeps track of the stamps collected based on the mission

---
+++ Assets/Scripts/StampProgressManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StampProgressManager : MonoBehaviour
+{
+    private int numStampsCollected = 0;
+    public TextMeshProUGUI text;
+
+    void OnEnable()
+    {
+        DisplayUpdate();
+    }
+
+    void DisplayUpdate()
+    {
+        numStampsCollected = ProgressManager.Instance.GetNumStampsUnlocked();
+        text.SetText(numStampsCollected.ToString());
+    }
+}
