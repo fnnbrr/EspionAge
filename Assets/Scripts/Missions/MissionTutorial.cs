b6e0b6100f319e60080806e0be54aa5d11db32ec
Alexander Jurcau <jurcaua@gmail.com>
2020-03-02 14:19:28 -0500

    New Tutorial Prototype Tuning + Code Cleanup

--- Assets/Scripts/Missions/MissionTutorial.cs
+++ Assets/Scripts/Missions/MissionTutorial.cs
@@ -7,18 +7,20 @@ using Cinemachine;
 public class MissionTutorial : AMission
 {
     public Vector3 playerStartPosition;
-    public GameObject startCutsceneCamera;
-    public GameObject startCutsceneText;
+
+    [Header("Cutscene")]
+    public GameObject vaseFocusCameraPrefab;
+    public GameObject vaseDropCutsceneText;
+    public GameObject enemyFocusCameraPrefab;
+    public GameObject enemyCutsceneText;
+    public GameObject birdieRunawayCutsceneText;
 
     [Header("Vase - General")]
     public GameObject vasePrefab;
     public GameObject vaseStandPrefab;
-    [Header("First Vase + Cutscene")]
 
+    [Header("First Vase")]
     public Vector3 firstVasePosition;
-    public GameObject vaseFocusCameraPrefab;
-    public float vaseFocusSeconds;
-    private GameObject vaseFocusCamInstance;
     private LoudObject firstVaseLoudObject;
     private BreakableObject firstVaseBreakable;
 
@@ -26,33 +28,44 @@ public class MissionTutorial : AMission
     public List<Vector3> vasePositions;
     private List<GameObject> spawnedVases;
     private List<GameObject> spawnedVaseStands;
-    // stand position must then be, (x - 0.5, 1.5, z - 1)
+    // stand position must then be, (x - 0.5, 1.5, z - 1) (empirically)
 
-    public MissionObject vase;
-    private LoudObject vaseLoudObject;
+    [Header("Chaser Enemies")]
+    public GameObject chaserPrefab;
+    public List<Vector3> enemyPositions;
 
-    [Header("Note")]
-    public MissionObject note;
-
-    // Distracting Senior
-    public MissionObject distractingSenior;
-    private NPCInteractable distractingSeniorNPCInteractable;
-    private float distractingSeniorDefaultBoundaryRadius;
+    //[Header("Note")]
+    //public MissionObject note;
 
     private bool startCutscenePlayed = false;
+
+    private List<GameObject> spawnedEnemies;
+    private List<GameObject> camerasToCleanUp;
 
     // General Logic Overview:
     // * start out faded out
     // * move player to the spawn point (in her room)
-    // * spawn the old person waiting 
-    // ~ mission giver at the end of the hallway
-    //   ~ somehow make the mission giver have a certain conversation
-    //   ~ somehow wait for the player to talk to the the mission giver to end the mission
-    // * spawn the breakable vase
+    // * spawn all the vases
+    // * mission giver at the end of the hallway
+    //   * somehow make the mission giver have a certain conversation
+    //   * somehow wait for the player to talk to the the mission giver to end the mission
     // * fade in
     // - start animation of note going under the door
     //   - this note should be an interactable which will have some text of birdie speaking to herself (reading outloud i guess)
+    // * for for birdie to exit to hallway before starting cutscene
+    //   * vase drops, birdie gets excited
+    //   * enemies come to yell at her
+    //   * birdie runs away
     // * finish mission once we finishing interacting with the mission giver (which will also give us the next mission)
+
+    private void Awake()
+    {
+        spawnedVases = new List<GameObject>();
+        spawnedVaseStands = new List<GameObject>();
+        spawnedEnemies = new List<GameObject>();
+        camerasToCleanUp = new List<GameObject>();
+    }
+
     protected override void Initialize()
     {
         Debug.Log("MissionTutorial Initialize()!");
@@ -63,28 +76,10 @@ public class MissionTutorial : AMission
         // Move player to a set position
         GameManager.Instance.GetPlayerTransform().position = playerStartPosition;
 
-        // Spawn the old person waiting, that'll annoy us once we drop the vase
-        //  set the boundary radius to 0 so they do not follow us
-        //distractingSenior.spawnedInstance = MissionManager.Instance.SpawnMissionObject(distractingSenior);
-        //distractingSenior.spawnedInstance.tag = Constants.TAG_ENEMY;
-        //distractingSeniorNPCInteractable = distractingSenior.spawnedInstance.GetComponent<NPCInteractable>();
-        //distractingSeniorDefaultBoundaryRadius = distractingSeniorNPCInteractable.boundaryRadius;
-        //distractingSeniorNPCInteractable.boundaryRadius = 0f;
-
         // Toggle the event in the EventManager
         GameEventManager.Instance.SetEventStatus(GameEventManager.GameEvent.TutorialActive, true);
-        // The old person waiting should already be in the world and be set up with a conditional conversation that'll end this mission
-        //  the logic there should be based on this GameEvent enum, and for now just end the current mission, since we know itll be the first thing
-        //  - cleaner way otherwise would possibily be having a mapping of some Mission enum to class types, where we can do something like:
-        //      - FindObjectOfType<class_type>(), the pass it into the MissionManager as per usual
 
-        // Spawn the vase (or is it already there?) and init the values there
-        //vase.spawnedInstance = MissionManager.Instance.SpawnMissionObject(vase);
-        //vaseLoudObject = vase.spawnedInstance.GetComponentInChildren<LoudObject>();
-        //vaseLoudObject.OnHit += OnVaseDrop;
-
-        spawnedVases = new List<GameObject>();
-        spawnedVaseStands = new List<GameObject>();
+        // spawn the specific first vase we will drop, and the remaining ones
         GameObject firstVaseInstance = SpawnVase(firstVasePosition);
         firstVaseLoudObject = firstVaseInstance.GetComponentInChildren<LoudObject>();
         firstVaseLoudObject.dropRadius = 0f;
@@ -106,48 +101,84 @@ public class MissionTutorial : AMission
 
     private void StartDropCutscene()
     {
+        RegionManager.Instance.hallway.OnPlayerEnter -= StartDropCutscene;
+        startCutscenePlayed = true;
         firstVaseLoudObject.Drop();
         firstVaseBreakable.OnBreak += StartVaseFocus;
     }
 
     private void StartVaseFocus(GameObject brokenInstance)
     {
-        StartCoroutine(VaseFocusCoroutine(brokenInstance));
+        StartCoroutine(VaseCutsceneCoroutine(brokenInstance));
     }
 
-    private IEnumerator VaseFocusCoroutine(GameObject focusObject)
+    private IEnumerator VaseCutsceneCoroutine(GameObject focusObject)
     {
-        vaseFocusCamInstance = CameraManager.Instance.SpawnCameraFromPrefab(vaseFocusCameraPrefab);
-        vaseFocusCamInstance.GetComponent<CinemachineVirtualCamera>().LookAt = focusObject.transform;
-
+        GameManager.Instance.GetPlayerController().EnablePlayerInput = false;
         CinemachineVirtualCamera currentCamera = CameraManager.Instance.GetActiveVirtualCamera();
-        CameraManager.Instance.BlendTo(vaseFocusCamInstance.GetComponent<CinemachineVirtualCamera>());
 
-        yield return new WaitForSeconds(vaseFocusSeconds);
+        // Cutscene Order: Vase Focus -> Enemies Come -> Birdie Runs Away
+        yield return StartCoroutine(PlayCutscenePart(currentCamera, vaseFocusCameraPrefab, vaseDropCutsceneText, focusObject.transform));
+        SpawnEnemies();
+        yield return StartCoroutine(PlayCutscenePart(currentCamera, enemyFocusCameraPrefab, enemyCutsceneText, spawnedEnemies[0].transform));
 
-        CameraManager.Instance.BlendTo(currentCamera);
-        CameraManager.Instance.OnBlendingComplete += DeleteAfterBlending;
+        GameManager.Instance.GetPlayerController().EnablePlayerInput = true;
+
+        yield return StartCoroutine(PlayCutsceneText(birdieRunawayCutsceneText));
     }
 
-    private void DeleteAfterBlending(CinemachineVirtualCamera fromCamera, CinemachineVirtualCamera toCamera)
+    private void SpawnEnemies()
     {
-        Destroy(vaseFocusCamInstance);
+        enemyPositions.ForEach(p =>
+        {
+            GameObject enemyInstance = Instantiate(chaserPrefab, p, Quaternion.identity);
+            spawnedEnemies.Add(enemyInstance);
+
+            PureChaser chaser = Utils.GetRequiredComponent<PureChaser>(enemyInstance);
+            chaser.targetTransform = GameManager.Instance.GetPlayerTransform();
+        });
+    }
+
+    private IEnumerator PlayCutscenePart(CinemachineVirtualCamera startCamera, GameObject cameraPrefab, GameObject cutsceneTextPrefab, Transform focusTransform)
+    {
+        GameObject cutsceneCameraInstance = CameraManager.Instance.SpawnCameraFromPrefab(cameraPrefab);
+        cutsceneCameraInstance.GetComponent<CinemachineVirtualCamera>().LookAt = focusTransform;
+        camerasToCleanUp.Add(cutsceneCameraInstance);
+        CameraManager.Instance.BlendTo(cutsceneCameraInstance.GetComponent<CinemachineVirtualCamera>(), alertGlobally: false);
+
+        yield return StartCoroutine(PlayCutsceneText(cutsceneTextPrefab));
+
+        CameraManager.Instance.BlendTo(startCamera, alertGlobally: false);
+        CameraManager.Instance.OnBlendingComplete += CleanupCamerasAfterBlending;
+    }
+
+    private IEnumerator PlayCutsceneText(GameObject cutsceneTextPrefab)
+    {
+        GameObject instantiatedCutsceneText = Instantiate(cutsceneTextPrefab, UIManager.Instance.mainUICanvas.transform);
+        Animator textAnimator = Utils.GetRequiredComponentInChildren<Animator>(instantiatedCutsceneText);
+        float textAnimationLength = textAnimator.GetCurrentAnimatorStateInfo(0).length;
+
+        yield return new WaitForSeconds(textAnimationLength);
+        Destroy(instantiatedCutsceneText);
+    }
+
+    private void CleanupCamerasAfterBlending(CinemachineVirtualCamera fromCamera, CinemachineVirtualCamera toCamera)
+    {
+        CameraManager.Instance.OnBlendingComplete += CleanupCamerasAfterBlending;
+        camerasToCleanUp.ForEach(c =>
+        {
+            Destroy(c);
+        });
     }
 
     private GameObject SpawnVase(Vector3 position)
     {
         GameObject vaseInstance = Instantiate(vasePrefab, position, Quaternion.identity);
+        Debug.Log(vaseInstance);
         spawnedVases.Add(vaseInstance);
         spawnedVaseStands.Add(Instantiate(vaseStandPrefab, new Vector3(position.x - 1f, 1.5f, position.z - 1f), Quaternion.identity));
 
         return vaseInstance;
-    }
-
-    private void OnVaseDrop()
-    {
-        vaseLoudObject.OnHit -= OnVaseDrop;
-        distractingSeniorNPCInteractable.boundaryRadius = distractingSeniorDefaultBoundaryRadius;
-        distractingSenior.spawnedInstance.tag = Constants.TAG_NONE;
     }
 
     protected override void Cleanup()
@@ -156,90 +187,41 @@ public class MissionTutorial : AMission
         //  this means its possible for some Singletons to have already been garbage collected by the time we get here
         Debug.Log("MissionTutorial Cleanup()!");
 
-        // Despawn the old person waiting for us
-
+        // Update GameEventManager
         if (GameEventManager.Instance)
         {
-            // Toggle the event in the EventManager
             GameEventManager.Instance.SetEventStatus(GameEventManager.GameEvent.TutorialActive, false);
         }
 
+        // Destroying the note
         if (MissionManager.Instance)
         {
-            // Goodbye distracting senior!
-            //MissionManager.Instance.DestroyMissionObject(distractingSenior);
-
-            // Despawn the vase (if it isnt already going to be there?)
-            //MissionManager.Instance.DestroyMissionObject(vase);
-
             // Delete the note if it still exists
             //MissionManager.Instance.DestroyMissionObject(note);
         }
 
+        // Handle the cutscene event handlers
         if (!startCutscenePlayed && RegionManager.Instance)
         {
-            //RegionManager.Instance.hallway.OnPlayerEnter -= StartCutscene;
+            RegionManager.Instance.hallway.OnPlayerEnter -= StartDropCutscene;
         }
         startCutscenePlayed = false;
 
-        spawnedVases.ForEach(v =>
-        {
-            Destroy(v);
-        });
-        spawnedVaseStands.ForEach(v =>
-        {
-            Destroy(v);
-        });
+        // Destroy all spawned objects
+        DestroyFromList(spawnedVases);
+        DestroyFromList(spawnedVaseStands);
+        DestroyFromList(spawnedEnemies);
+        DestroyFromList(camerasToCleanUp);
     }
 
-    private void StartCutscene()
+    private void DestroyFromList(List<GameObject> gameObjects)
     {
-        // The current assumptions are gonna be that:
-        //  - we will have a camera where we need to set the look at
-        //  - and it will have an already created Animator with a single state that it will start on Awake
-        //
-        // So we can just destroy it after the animation time is over and it should switch back to whatever the current camera was
-        RegionManager.Instance.hallway.OnPlayerEnter -= StartCutscene;
-
-        if (startCutscenePlayed) return;
-        startCutscenePlayed = true;
-
-        if (!startCutsceneCamera || !startCutsceneText) return;
-
-        GameManager.Instance.GetPlayerController().EnablePlayerInput = false;
-
-        // Setting up the cutscene camera
-        GameObject instantiatedCutscenePrefab = Instantiate(startCutsceneCamera);
-        CinemachineVirtualCamera virtualCamera = instantiatedCutscenePrefab.GetComponentInChildren<CinemachineVirtualCamera>();
-        Animator virtualCameraAnim = instantiatedCutscenePrefab.GetComponentInChildren<Animator>();
-        if (!virtualCamera || !virtualCameraAnim) return;  // early exit
-        virtualCamera.LookAt = distractingSenior.spawnedInstance.transform;
-        float cameraAnimationLength = virtualCameraAnim.GetCurrentAnimatorStateInfo(0).length * 1.5f;
-
-        // Setting up the cutscene text
-        GameObject instantiatedCutsceneText = Instantiate(startCutsceneText, UIManager.Instance.mainUICanvas.transform);
-        Animator textAnimator = instantiatedCutsceneText.GetComponentInChildren<Animator>();
-        if (!textAnimator) return;
-        float textAnimationLength = textAnimator.GetCurrentAnimatorStateInfo(0).length;
-
-        StartCoroutine(EndCutsceneOperations(instantiatedCutscenePrefab, instantiatedCutsceneText, Mathf.Max(cameraAnimationLength, textAnimationLength)));
-    }
-
-    private IEnumerator EndCutsceneOperations(GameObject cutsceneObject, GameObject cutsceneText, float cutsceneLength)
-    {
-        yield return new WaitForSeconds(cutsceneLength);
-
-        UIManager.Instance.FadeOut();
-
-        yield return new WaitForSeconds(UIManager.Instance.fadeSpeed);
-
-        Destroy(cutsceneObject);
-        Destroy(cutsceneText);
-
-        UIManager.Instance.FadeIn();
-
-        GameManager.Instance.GetPlayerController().EnablePlayerInput = true;
-
-        UIManager.Instance.staminaBar.Glow();
+        if (gameObjects != null && gameObjects.Count > 0)
+        {
+            gameObjects.ForEach(v =>
+            {
+                Destroy(v);
+            });
+        }
     }
 }
---
+++ Assets/Scripts/NPCs/PureChaser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PureChaser : MonoBehaviour
+{
+    public Transform targetTransform;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = Utils.GetRequiredComponent<NavMeshAgent>(this);
+    }
+
+    protected void ChaseTarget()
+    {
+        if (targetTransform)
+        {
+            agent.SetDestination(targetTransform.position);
+        }
+    }
+
+    private void Update()
+    {
+        if (!agent.isOnNavMesh) return;
+
+        ChaseTarget();
+    }
+}
