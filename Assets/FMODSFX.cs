0fa3acde4d95ba621e83c6d2cccd02b233d43c8f
Alexander Jurcau <jurcaua@gmail.com>
2020-03-11 15:00:11 -0400

    SFX Script Cleanup

---
+++ Assets/FMODSFX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODSFX : MonoBehaviour
+{
+    void FMODPlayOneShot(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path);
+        Debug.Log($"Triggered OneShot: {path}");
+    }
+}
