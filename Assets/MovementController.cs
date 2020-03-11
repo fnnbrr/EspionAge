1b59f1b36d5b5b1dc283bfbfb5de5a16ddcf7a69
Jeppehz <cruz.julianp@gmail.com>
2020-03-11 04:48:22 -0400

    Added dash sound to movementcontroller script
    Just moved from playercontroller to movementcontroller

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -15,6 +15,9 @@ public class MovementController : MonoBehaviour
     private Rigidbody rb;
     private Animator anim;
     private Vector3 movement;
+    [FMODUnity.EventRef]
+    public string boost;
+
 
     [Header("Special / Dash")]
     public float dashForce;
@@ -70,6 +73,7 @@ public class MovementController : MonoBehaviour
     private void PerformDash()
     {
         rb.AddForce(transform.forward * dashForce, ForceMode.Impulse);
+        FMODUnity.RuntimeManager.PlayOneShot(boost, transform.position);
     }
 
     void FixedUpdate()
