afd62ed213cd815259f1d938535edc0a24ca72f2
masonachu <masonachu@gmail.com>
2020-03-15 01:53:18 -0400

    imported MasonVic prefabs and scripts

---
+++ Assets/Scripts/Audio/FMODMissionComplete.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODMissionComplete : MonoBehaviour
+{
+    [FMODUnity.ParamRef]
+    public string param;
+    private bool completed;
+    private bool SeekSpeed = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" == !completed)
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(param, 1f, SeekSpeed);
+            completed = true;
+            Debug.Log("param 1f");
+            ifCompleted();
+        }
+    }
+
+    void ifCompleted ()
+    {
+        if (completed)
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(param, 0f);
+            completed = false;
+            Debug.Log("param 0f");
+        }
+    }
+}
