f24e7049aed5d750fca223a0dd6292ef0fa29209
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-03-17 01:35:47 -0400

    Improved repositioning of speechbubble to use width of speechBubble outline and fit the height of speech container

--- Assets/Scripts/Interactables/Dialogue/SpeakerUI.cs
+++ Assets/Scripts/Interactables/Dialogue/SpeakerUI.cs
@@ -6,19 +6,27 @@ using TMPro;
 
 public class SpeakerUI : MonoBehaviour
 {
+    public GameObject speechBubbleOutline;
     public GameObject textBoxContainer;
     public GameObject canvas;
     public TextMeshProUGUI characterName;
     public TextMeshProUGUI conversationText;
 
     private RectTransform textBoxRect;
+    private RectTransform bubbleOutlineRect;
     private Vector2 tbAnchor;
     
     private Vector3 textPosition;
 
+    // Used for padding on right side of screen for repositioning when resizing
+    public float extraXPadding = 20f;
+
     private void Awake()
     {
+        // textBoxRect is needed to for the anchor position and outlineRect is used to find the width of
+        // the resizable speech bubble because textBoxRect is the maxWidth
         textBoxRect = Utils.GetRequiredComponent<RectTransform>(textBoxContainer);
+        bubbleOutlineRect = Utils.GetRequiredComponent<RectTransform>(speechBubbleOutline);
     }
 
     void Start()
@@ -33,7 +41,7 @@ public class SpeakerUI : MonoBehaviour
 
         tbAnchor = textBoxRect.anchoredPosition;
 
-        tbAnchor.x = Mathf.Clamp(tbAnchor.x, 0, Mathf.Max(0, Screen.width - textBoxRect.sizeDelta.x));
+        tbAnchor.x = Mathf.Clamp(tbAnchor.x, 0, Mathf.Max(0, Screen.width - bubbleOutlineRect.sizeDelta.x - extraXPadding));
         tbAnchor.y = Mathf.Clamp(tbAnchor.y, Mathf.Min(-Screen.height + textBoxRect.sizeDelta.y, 0), 0);
 
         textBoxRect.anchoredPosition = tbAnchor;
