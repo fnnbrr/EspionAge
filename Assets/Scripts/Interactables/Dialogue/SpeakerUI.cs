c002b7244d975f5dbda2ace313610cd5bf41f357
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-03-16 20:11:47 -0400

    Dialogue Convos textboxes should are now always repositioned if they were to go off screen

--- Assets/Scripts/Interactables/Dialogue/SpeakerUI.cs
+++ Assets/Scripts/Interactables/Dialogue/SpeakerUI.cs
@@ -10,11 +10,17 @@ public class SpeakerUI : MonoBehaviour
     public GameObject canvas;
     public TextMeshProUGUI characterName;
     public TextMeshProUGUI conversationText;
+
+    private RectTransform textBoxRect;
+    private Vector2 tbAnchor;
+    private float anchorX;
+    private float anchorY;
     
     private Vector3 textPosition;
 
     void Start()
     {
+        textBoxRect = Utils.GetRequiredComponent<RectTransform>(textBoxContainer);
         Hide();
     }
 
@@ -22,6 +28,18 @@ public class SpeakerUI : MonoBehaviour
     {
         textPosition = Camera.main.WorldToScreenPoint(transform.position);
         textBoxContainer.transform.position = textPosition;
+
+        tbAnchor = textBoxRect.anchoredPosition;
+        anchorX = tbAnchor.x;
+        anchorY = tbAnchor.y;
+
+        anchorX = Mathf.Clamp(anchorX, 0, Mathf.Max(0, Screen.width - textBoxRect.sizeDelta.x));
+        anchorY = Mathf.Clamp(anchorY, -Screen.height + textBoxRect.sizeDelta.y, 0);
+
+        tbAnchor.x = anchorX;
+        tbAnchor.y = anchorY;
+
+        textBoxRect.anchoredPosition = tbAnchor;
     }
 
     public void Show()
