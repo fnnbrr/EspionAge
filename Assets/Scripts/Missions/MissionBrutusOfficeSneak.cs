721f7d5b8feed824d0264b3861b4ca59b0e6be32
Alexander Jurcau <jurcaua@gmail.com>
2020-03-26 15:34:46 -0400

    Brutus office enter cutscene + simple logic

--- Assets/Scripts/Missions/MissionBrutusOfficeSneak.cs
+++ Assets/Scripts/Missions/MissionBrutusOfficeSneak.cs
@@ -1,12 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class MissionBrutusOfficeSneak : AMission
 {
+    public PlayableDirector onEnterCutscene;
+
     protected override void Initialize()
     {
         Debug.Log("Starting mission: MissionBrutusOfficeSneak");
+
+        RegionManager.Instance.OnPlayerEnterZone += OnPlayerEnterOffice;
+    }
+
+    private void OnPlayerEnterOffice(CameraZone zone)
+    {
+        if (zone == RegionManager.Instance.brutusOffice)
+        {
+            RegionManager.Instance.OnPlayerEnterZone -= OnPlayerEnterOffice;
+
+            onEnterCutscene.Play();
+            StartCoroutine(DisablePlayerMovementDuringCutscene(onEnterCutscene));
+        }
+    }
+
+    private IEnumerator DisablePlayerMovementDuringCutscene(PlayableDirector cutsceneDirector)
+    {
+        GameManager.Instance.GetPlayerController().EnablePlayerInput = false;
+
+        while(cutsceneDirector.state == PlayState.Playing)
+        {
+            yield return null;
+        }
+
+        GameManager.Instance.GetPlayerController().EnablePlayerInput = true;
     }
 
     protected override void Cleanup()
