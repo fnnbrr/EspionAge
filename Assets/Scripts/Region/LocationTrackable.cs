b232a17b305d95b292d330031433c3847d80ff8c
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-03-31 19:45:42 -0400

    revert changes to location tracker

--- Assets/Scripts/Region/LocationTrackable.cs
+++ Assets/Scripts/Region/LocationTrackable.cs
@@ -4,13 +4,7 @@ public class LocationTrackable : MonoBehaviour
 {
     private void OnEnable()
     {
-        Debug.Log(gameObject.name);
-        if (RegionManager.Instance)
-        {
-            RegionManager.Instance.RegisterTrackedObject(gameObject);
-            Debug.Log("added");
-            Debug.Log(RegionManager.Instance.trackedObjectZones[gameObject][0]);
-        }
+        if (RegionManager.Instance) RegionManager.Instance.RegisterTrackedObject(gameObject);
     }
 
     private void OnDisable()
