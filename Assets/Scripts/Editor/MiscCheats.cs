bf902a8c27c9c775cd24638d99072feec3896cd6
Alexander Jurcau <jurcaua@gmail.com>
2020-04-03 16:31:27 -0400

    Useful time scale changing cheats for quick testing

---
+++ Assets/Scripts/Editor/MiscCheats.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MiscCheats : MonoBehaviour
+{
+    public static bool Validate()
+    {
+        return Application.isPlaying;
+    }
+
+    public static void SetTimeScale(float amount)
+    {
+        Time.timeScale = amount;
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_SLOWMOTION, true)]
+    public static bool ValidateSetTimeScaleSlow()
+    {
+        return Validate();
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_SLOWMOTION)]
+    public static void SetTimeScaleSlow()
+    {
+        SetTimeScale(0.5f);
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_REGULAR, true)]
+    public static bool ValidateSetTimeScaleRegular()
+    {
+        return Validate();
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_REGULAR)]
+    public static void SetTimeScaleRegular()
+    {
+        SetTimeScale(1f);
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_DOUBLE, true)]
+    public static bool ValidateSetTimeScaleDouble()
+    {
+        return Validate();
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_DOUBLE)]
+    public static void SetTimeScaleDouble()
+    {
+        SetTimeScale(2f);
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_TRIPLE, true)]
+    public static bool ValidateSetTimeScaleTriple()
+    {
+        return Validate();
+    }
+
+    [MenuItem(Constants.CHEATS_MISC_TIME_TRIPLE)]
+    public static void SetTimeScaleTriple()
+    {
+        SetTimeScale(3f);
+    }
+}
